568fc0559263f4ae95c89a2794f886dd2b638b3b
Satyabrata Das <satyabrata_pw1_135@masai.school>
2021-11-28 20:48:14 +0530

    simple timer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Timer from './Timer/Timer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple Timer</h1>
+      <Timer initial={1} final={10}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Timer/Timer.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+export default function Timer({initial,final}){
+    const [timer,setTimer]=useState(initial);
+
+    useEffect(()=>{
+        const id=setInterval(()=>{
+            setTimer((prev)=>{
+                if(prev===final-1)
+                {
+                    clearInterval(id);
+                }
+                return prev+1;
+            })
+        },1000)
+        return ()=>{
+            clearInterval(id);
+        }
+    },[final])
+    return(
+        <>
+          <div>Initial Time: {initial} Second</div>
+          <div>Final Time: {final} Second</div>
+          <div className="container">
+          <h1>Timer: {timer} Sec</h1>
+          </div>
+        </>
+    )
+}
+
+   
